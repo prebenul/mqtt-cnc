63277fbf63bcab86173e396726e472701215bdc8
prebenul <preben.ulltang@gmail.com>
2020-08-31 06:00:54 +0200

    Create create_database.sql

---
+++ create_database.sql
@@ -0,0 +1,8 @@
+dbc_4 	CREATE TABLE `dbc_4` (
+ `id` int(15) NOT NULL AUTO_INCREMENT,
+ `row` int(15) NOT NULL,
+ `width` float NOT NULL,
+ `outerdiam` float NOT NULL,
+ `segs` int(11) NOT NULL,
+ PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4
