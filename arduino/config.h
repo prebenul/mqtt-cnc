544b9e04be7ae5cae2ce673f4698f2a880ea9d98
prebenul <preben.ulltang@gmail.com>
2020-08-31 20:05:55 +0200

    Create config.h

---
+++ arduino/config.h
@@ -0,0 +1,6 @@
+#define SECRET_SSID "4G-Mobile-WiFi-E509"
+#define SECRET_PASS "bb89eigm"
+#define MQTT_ADDRESS "192.168.8.105"
+#define MQTT_USERNAME "espmqtt"
+#define MQTT_PASS "lego1337"
+int mqttPort = 1883;
